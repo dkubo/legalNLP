6e02ac5bd58be21b979a8c6ba315b6973887ca7b
magnesium <daiki.kosen.10@gmail.com>
2016-06-20 19:19:06 +0900

    test

--- hanrei/src/a.rb
+++ hanrei/src/a.rb
@@ -16,10 +16,11 @@ def main()
 	hash=Hash.new(0)
 	cnt=0
 	sum_cnt=0
+	id_list=[]
 	sql="select id from hanrei"
 	cursorID=db.executeSQL(sql)
 	cursorID.each do |tuple_id|
-#		puts "---------------------------"
+		puts "---------------------------"
 		id_ = tuple_id[0]
 		sql="select riyuPart,summary,trialYear from hanrei where id=#{id_}"
 		cursor=db.executeSQL(sql)
@@ -30,19 +31,17 @@ def main()
 			if summary != nil then
 				summary=summary.delete(" 　").strip
 				if "" != summary then
+					id_list.push(id_)
 					sum_cnt+=1
-					hash[trialYear]+=1
+#					hash[trialYear]+=1
 				end
 			end
-#			text += syubun.delete("\n")
-#			text += riyu.delete("\n")
-#			sp=SenSplit.new(text)
-#			result=sp.split_period()
 		end
 		cnt+=1
-		for k,v in hash do
-			puts k+"："+v.to_s+"/"+cnt.to_s
-		end
+		puts id_list
+#		for k,v in hash do
+#			puts k+"："+v.to_s
+#		end
 	end
 	db.closeDB
 end
