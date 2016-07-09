2a0d9833b440ecccb7dcef63f1596708f8fd20ef
magnesium <daiki.kosen.10@gmail.com>
2016-07-09 14:24:27 +0900

    update  count.jl

--- MWE/src/count.jl
+++ MWE/src/count.jl
@@ -21,53 +21,46 @@ const JDMWE_PATH="../data/JDMWE_idiom v1.3 20121215.xlsx"
 
 
 
-#jdmwe = readxlsheet(DataFrame, JDMWE_PATH, "Sheet1", colnames=[:A, :B, :C, :D, :E, :F, :G, :H])
-#println(jdmwe)
-
 #トライ木:http://datastructuresjl.readthedocs.io/en/latest/trie.html?highlight=trie
 
+#BCCWJ_CORE_M-XMLファイルオープン
+#child = readdir(BCCWJ_ROOT)
+#for xfile in child
+##	xfile="OC01_00001.xml"
+#	println("--------------------------")
+#	path = BCCWJ_ROOT*"/"*xfile
+#	doc = open(path) do fp
+#		readall(fp)
+#	end
+#	#xmlパーズ
+#	root_node = xp_parse(doc)
+#	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
+#	for sentence_node in sentence_nodes
+#		println("--------------------------")
+#		sentence = []
+#		yomi_sentence = []
+#		yomi = ""
+#		suw = ""
+#		sentence_node = xp_parse(string(sentence_node))
+#		suw_nodes = LibExpat.find(sentence_node,"/sentence//SUW")
+#		for suw_node in suw_nodes
+#			suw = suw_node.elements[1]
+#			push!(sentence,suw)
+#			try
+#				yomi = suw_node.attr["kana"]
+#			catch e
+#				yomi = suw_node.attr["formBase"]
+#			end			
+#			push!(yomi_sentence,jcconv.kata2hira(yomi))
+#		end
+##		@show path
+##		@show sentence
+##		@show yomi_sentence
+#	end
+#end
 
-#bccwj = readtable(BCCWJ_PATH, separator='\t', header=false, names=[:register, :sample_id, :chara_start_posi, :chara_end_posi, :order_id, :occur_start_posi, 
-#																																		:occur_end_posi, :fixed_frg, :variable_frg, :BI_label, :lemma_table_id, :lemma_id, :lemma, 
-#																																		:lForm, :subLemma, :wType, :pos, :cType, :cForm, :formBase, :usage, :orthBase, :orthToken,
-#																																		:originalText ,:pronToken])
-#println(bccwj)
-
-#XMLファイルオープン
-child = readdir(BCCWJ_ROOT)
-for xfile in child
-#	xfile="OC01_00001.xml"
-	println("--------------------------")
-	path = BCCWJ_ROOT*"/"*xfile
-	doc = open(path) do fp
-		readall(fp)
-	end
-	#xmlパーズ
-	root_node = xp_parse(doc)
-	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
-	for sentence_node in sentence_nodes
-		println("--------------------------")
-		sentence = ""
-		yomi_sentence = ""
-		yomi = ""
-		suw = ""
-		sentence_node = xp_parse(string(sentence_node))
-		suw_nodes = LibExpat.find(sentence_node,"/sentence//SUW")
-		for suw_node in suw_nodes
-			suw = suw_node.elements[1]
-			sentence *= suw			
-			try
-				yomi = suw_node.attr["kana"]
-			catch e
-				yomi = suw_node.attr["formBase"]
-			end
-			yomi_sentence *= yomi
-		end
-#		@show path
-		@show sentence
-		@show jcconv.kata2hira(yomi_sentence)
-	end
-end
+jdmwe = readxlsheet(DataFrame, JDMWE_PATH, "Sheet1", colnames=[:A, :B, :C, :D, :E, :F, :G, :H])
+println(jdmwe)
 
 #open(file) do bccwj
 #	sentence=""
