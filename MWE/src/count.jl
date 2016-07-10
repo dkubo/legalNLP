fd8b1ebd8261b86b047e118e6844035d5c6e244c
Kubo Daiki <daiki-ku@pine32.naist.jp>
2016-07-10 22:16:14 +0900

    update count

--- MWE/src/count.jl
+++ MWE/src/count.jl
@@ -4,6 +4,7 @@ using ExcelReaders
 using PyCall
 using LibExpat
 using DataFrames
+using DataStructures
 @pyimport jcconv
 
 #using LightXML
@@ -25,7 +26,8 @@ const JDMWE_PATH="../data/JDMWE_idiom v1.3 20121215.xlsx"
 #	JDMWE読込
 ################################################
 function loadMWE()
-	mwe_dict = []
+	trie = Trie{Int}()			#トライ木宣言
+	cnt = 1
 	jdmwe_df = readxlsheet(DataFrame, JDMWE_PATH, "Sheet1", colnames=[:A, :B, :C, :D, :E, :F, :G, :H])
 	#println(jdmwe_df[[:B,:E]])
 
@@ -34,76 +36,77 @@ function loadMWE()
 	#.:内部修飾, _:異字種で表記可能
 	for mwe in jdmwe_df[:B]
 	#	println(split(mwe,"-"))
-		mwe = replace(mwe,r"(^\.|_)","")		#_と先頭の.を消去
-		mwe = replace(mwe,".","-.-")		#先頭以外の.を-.-に置換	
-		mwe = split(mwe,"-")
-#		push!(mwe_dict,filter(e->e≠"",mwe))
-		push!(mwe_dict,mwe)
+		mwe = replace(mwe,r"(^\.|_|-)","")		#_と先頭の.を消去
+#		mwe = replace(mwe,".","-.-")		#先頭以外の.を-.-に置換	
+#		mwe = split(mwe,"-")
+		trie[mwe] = cnt
+		cnt += 1
 	end
-	println(jdmwe_df[:B])
-	println(mwe_dict)
-	return mwe_dict
+	return trie
 end
 
-mwe_dict = loadMWE()
+trie = loadMWE()
+@show trie
+@show subtrie(trie, "あいそ")
+
 #BCCWJ_CORE_M-XMLファイルオープン
-child = readdir(BCCWJ_ROOT)
-for xfile in child
-	println("--------------------------")
-	path = BCCWJ_ROOT*"/"*xfile
-	doc = open(path) do fp
-		readall(fp)
-	end
-	#xmlパーズ
-	root_node = xp_parse(doc)
-	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
-	for sentence_node in sentence_nodes
-		println("--------------------------")
-		######################################
-		#		初期化
-		######################################
-		origin_sentence = []
-#		yomi_sentence = ""
-		yomi_sentence = []
-		yomi = ""
-		suw = ""
-		######################################
-		#		XMLパーズ
-		######################################
-		sentence_node = xp_parse(string(sentence_node))
-		suw_nodes = LibExpat.find(sentence_node,"/sentence//SUW")
-		for suw_node in suw_nodes
-			suw = suw_node.elements[1]
-			push!(origin_sentence,suw)
-			try
-				yomi = suw_node.attr["kana"]
-			catch e
-				yomi = suw_node.attr["formBase"]
-			end
-			push!(yomi_sentence,filter(e->e≠"",jcconv.kata2hira(yomi)))
-		end
-
-		######################################
-		#		マッチング
-		######################################
-#		for mwe in mwe_dict
-#			mwe_frg = 0
-#			for mwe_part in mwe
-#				for yomi in yomi_sentence
-#	#				reg = Regex(mwe)
-#					if yomi == mwe_part
-#						mwe_frg = 1
-#						@show mwe_part
-#						@show yomi_sentence
-#					else
-#						mwe_frg = 0					
-#					end					
-#	#				if ismatch(reg, yomi) == true
-#				end
-#			end
+#child = readdir(BCCWJ_ROOT)
+#for xfile in child
+#	println("--------------------------")
+#	path = BCCWJ_ROOT*"/"*xfile
+#	doc = open(path) do fp
+#		readall(fp)
+#	end
+#	#xmlパーズ
+#	root_node = xp_parse(doc)
+#	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
+#	for sentence_node in sentence_nodes
+#		println("--------------------------")
+#		######################################
+#		#		初期化
+#		######################################
+#		origin_sentence = []
+##		yomi_sentence = ""
+#		yomi_sentence = []
+#		yomi = ""
+#		suw = ""
+#		######################################
+#		#		XMLパーズ
+#		######################################
+#		sentence_node = xp_parse(string(sentence_node))
+#		suw_nodes = LibExpat.find(sentence_node,"/sentence//SUW")
+#		for suw_node in suw_nodes
+#			suw = suw_node.elements[1]
+#			push!(origin_sentence,suw)
+##			try
+##				yomi = suw_node.attr["kana"]
+##			catch e
+##				yomi = suw_node.attr["formBase"]
+##			end
+##			push!(yomi_sentence,filter(e->e≠"",jcconv.kata2hira(yomi)))
 #		end
-	end
-end
 
+#		######################################
+#		#		マッチング
+#		######################################
+##		for mwe in mwe_dict
+##			mwe_frg = 0
+##			for mwe_part in mwe
+##				reg = Regex(mwe_part)
+##				for yomi in yomi_sentence
+##					if yomi == mwe_part
+##						mwe_frg = 1
+##						@show mwe_part
+##						@show yomi_sentence
+##					else
+##						mwe_frg = 0					
+##					end
+##					
+##					if ismatch(reg, yomi) == true
+##				end
+##			end
+##		end
+#	end
+#end
 
 
