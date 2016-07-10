b8fe9033f850124a4025fdf87e26f7d428f2fe07
magnesium <daiki.kosen.10@gmail.com>
2016-07-10 14:36:54 +0900

    update script

--- MWE/src/count.jl
+++ MWE/src/count.jl
@@ -19,62 +19,65 @@ Daiki Kubo
 const BCCWJ_ROOT="../data/core_M-XML"
 const JDMWE_PATH="../data/JDMWE_idiom v1.3 20121215.xlsx"
 
+#trie: https://github.com/JuliaLang/DataStructures.jl/blob/master/src/trie.jl
+
 ########################
 #		JDMWE読込
 ########################
-jdmwe_df = readxlsheet(DataFrame, JDMWE_PATH, "Sheet1", colnames=[:A, :B, :C, :D, :E, :F, :G, :H])
-#println(jdmwe_df[[:B,:E]])
+function loadMWE()
+	mwe_dict = []
+	jdmwe_df = readxlsheet(DataFrame, JDMWE_PATH, "Sheet1", colnames=[:A, :B, :C, :D, :E, :F, :G, :H])
+	#println(jdmwe_df[[:B,:E]])
 
-#まずは品詞の制約なし
-#println(length(jdmwe_df[:B]))		#4449
-#.:内部修飾, _:異字種で表記可能
-for mwe in jdmwe_df[:B]
-#	println(split(mwe,"-"))
-	mwe = replace(mwe,"_","")		#_を消去
-	mwe = replace(mwe,".","-.-")		#先頭以外の.を-.-に置換	
-	mwe = split(mwe,"-")
-	filter!(e->e≠"",mwe)
-	println(mwe)
+	#まずは品詞の制約なし
+	#println(length(jdmwe_df[:B]))		#4449
+	#.:内部修飾, _:異字種で表記可能
+	for mwe in jdmwe_df[:B]
+	#	println(split(mwe,"-"))
+		mwe = replace(mwe,"_","")		#_を消去
+		mwe = replace(mwe,".","-.-")		#先頭以外の.を-.-に置換	
+		mwe = split(mwe,"-")
+		push!(mwe_dict,filter(e->e≠"",mwe))
+	end
+	return mwe_dict
 end
-#トライ木:http://datastructuresjl.readthedocs.io/en/latest/trie.html?highlight=trie
 
-########################
-#		BCCWJ読込
-########################
+mwe_dict = loadMWE()
+#println(mwe_dict)
 #BCCWJ_CORE_M-XMLファイルオープン
-#child = readdir(BCCWJ_ROOT)
+child = readdir(BCCWJ_ROOT)
 #for xfile in child
-##	xfile="OC01_00001.xml"
-#	println("--------------------------")
-#	path = BCCWJ_ROOT*"/"*xfile
-#	doc = open(path) do fp
-#		readall(fp)
-#	end
-#	#xmlパーズ
-#	root_node = xp_parse(doc)
-#	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
-#	for sentence_node in sentence_nodes
-#		println("--------------------------")
-#		sentence = []
-#		yomi_sentence = []
-#		yomi = ""
-#		suw = ""
-#		sentence_node = xp_parse(string(sentence_node))
-#		suw_nodes = LibExpat.find(sentence_node,"/sentence//SUW")
-#		for suw_node in suw_nodes
-#			suw = suw_node.elements[1]
-#			push!(sentence,suw)
-#			try
-#				yomi = suw_node.attr["kana"]
-#			catch e
-#				yomi = suw_node.attr["formBase"]
-#			end			
-#			push!(yomi_sentence,jcconv.kata2hira(yomi))
-#		end
-##		@show path
-##		@show sentence
-##		@show yomi_sentence
-#	end
+	xfile="OC01_00001.xml"
+	println("--------------------------")
+	path = BCCWJ_ROOT*"/"*xfile
+	doc = open(path) do fp
+		readall(fp)
+	end
+	#xmlパーズ
+	root_node = xp_parse(doc)
+	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
+	for sentence_node in sentence_nodes
+		println("--------------------------")
+		sentence = []
+		yomi_sentence = []
+		yomi = ""
+		suw = ""
+		sentence_node = xp_parse(string(sentence_node))
+		suw_nodes = LibExpat.find(sentence_node,"/sentence//SUW")
+		for suw_node in suw_nodes
+			suw = suw_node.elements[1]
+			push!(sentence,suw)
+			try
+				yomi = suw_node.attr["kana"]
+			catch e
+				yomi = suw_node.attr["formBase"]
+			end
+			push!(yomi_sentence,jcconv.kata2hira(yomi))
+		end
+#		@show path
+#		@show sentence
+#		@show yomi_sentence
+	end
 #end
 
 
