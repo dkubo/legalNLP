c5200aa44b9a3f99a4ec924aca2d7f9d4cc537b0
Kubo Daiki <daiki-ku@pine31.naist.jp>
2016-07-11 16:51:24 +0900

    update count.jl

--- MWE/src/count.jl
+++ MWE/src/count.jl
@@ -24,13 +24,14 @@ const JDMWE_PATH="../data/JDMWE_idiom v1.3 20121215.xlsx"
 #	JDMWE読込
 ################################################
 function loadMWE()
+	lex_cnt = 0				#MWEエントリ数カウント
 	trie = Trie{Int}()			#トライ木宣言
 	cnt = 1
 	jdmwe_df = readxlsheet(DataFrame, JDMWE_PATH, "Sheet1", colnames=[:A, :B, :C, :D, :E, :F, :G, :H])
 	#println(jdmwe_df[[:B,:E]])
 
 	#まずは品詞の制約なし
-	#println(length(jdmwe_df[:B]))		#4449
+	lex_cnt = length(jdmwe_df[:B])		#4449
 	#.:内部修飾, _:異字種で表記可能
 	for mwe in jdmwe_df[:B]
 	#	println(split(mwe,"-"))
@@ -40,7 +41,7 @@ function loadMWE()
 		trie[mwe] = cnt
 		cnt += 1
 	end
-	return trie
+	return trie,lex_cnt
 end
 
 ################################################
@@ -48,36 +49,44 @@ end
 ################################################
 function search_trie(trie,yomi_sentence)
 	match_mwe = ""
+	pmatch = ""				#マッチした部分下のsubtrie文字列
 	match_yomi = []
 	for yomi in yomi_sentence
-		buff = keys_with_prefix(trie, yomi)
-		if length(buff) == 1		#マッチ
-			trie = subtrie(trie, yomi)
-			match_mwe *= yomi
-			push!(match_yomi,yomi)
+		if yomi != ""
+			buff = keys_with_prefix(trie, yomi)
+			if length(buff) == 1		#マッチ
+				trie = subtrie(trie, yomi)
+				match_mwe *= yomi
+				push!(match_yomi,yomi)
+			end
 		end
 	end
-	if length(keys(trie)) != 1			#完全マッチでない場合
+	try
+		pmatch = keys(trie)			#完全マッチでない場合
+	catch
 		match_mwe = ""
 	end
+	if pmatch != ""
+		match_mwe = ""		
+	end
 	#yomi_sentenceからマッチ要素を削除
-	deleteat!(yomi_sentence, findin(yomi_sentence,[match_yomi]))	
-	@show yomi_sentence
+	deleteat!(yomi_sentence, findin(yomi_sentence,match_yomi))	
+#	@show yomi_sentence
 	return match_mwe,yomi_sentence
 end
-
 
 ################################################
 #	main
 ################################################
-trie = loadMWE()
+trie,lex_cnt = loadMWE()
+sen_cnt = 0		#文数カウント
 #@show trie
 #@show subtrie(trie, "")	#trieが返ってくる
 
 #BCCWJ_CORE_M-XMLファイルオープン
 child = readdir(BCCWJ_ROOT)
 for xfile in child
-	println("--------------------------")
+#	println("--------------------------")
 	path = BCCWJ_ROOT*"/"*xfile
 	doc = open(path) do fp
 		readall(fp)
@@ -86,7 +95,9 @@ for xfile in child
 	root_node = xp_parse(doc)
 	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
 	for sentence_node in sentence_nodes
-		println("--------------------------")
+		sen_cnt += 1
+		@show sen_cnt
+#		println("--------------------------")
 		######################################
 		#		初期化
 		######################################
@@ -112,12 +123,17 @@ for xfile in child
 		######################################
 		#		マッチング
 		######################################
-		match,yomi_sentence = search_trie(trie,yomi_sentence)
-#		push!(match_array,match)
-#		while match != ""		#他のMWEがマッチする可能性も考慮
-#			match = search_trie(trie,yomi_sentence)
-#			push!(match_array,match)
-#		end
+		match_mwe,yomi_sentence = search_trie(trie,yomi_sentence)
+		if match_mwe != ""				#なんらかのMWEにマッチした場合
+			push!(match_array,match_mwe)
+			@show match_array
+			while match_mwe != ""		#他のMWEがマッチする可能性も考慮
+				match_mwe,yomi_sentence = search_trie(trie,yomi_sentence)
+				if match_mwe != ""
+					push!(match_array,match_mwe)
+				end
+			end
+		end
 #		@show origin_sentence
 #		@show yomi_sentence
 #		@show match_array
