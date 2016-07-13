15c76e9fcec187e0d24ab4168ce34c378774127a
magnesium <daiki.kosen.10@gmail.com>
2016-07-13 17:03:09 +0900

    added ntt_si

--- MWE/src/count_intmod.jl
+++ MWE/src/count_intmod.jl
@@ -36,7 +36,7 @@ function loadMWE()
 	cnt = 1
 	reg_hash = Dict{UTF8String,Regex}()	#k:mwe,v:mwe_reg
 	jdmwe_df = readxlsheet(DataFrame, JDMWE_PATH, "Sheet1", colnames=[:A, :B, :C, :D, :E, :F, :G, :H])
-	@show (jdmwe_df[[301:302,1742:1743,1841:1842,2044:2045,2199:2200,2586:2587,2668:2669,2956:2957,3686:3687,3831:3832,3836:3837],[:A,:B,:C,:D,:E,:F,:G,:H]])
+	#@show (jdmwe_df[[301:302,1742:1743,1841:1842,2044:2045,2199:2200,2586:2587,2668:2669,2956:2957,3686:3687,3831:3832,3836:3837],[:A,:B,:C,:D,:E,:F,:G,:H]])
 #	@show (jdmwe_df[[:B,:E]])
 
 	#まずは品詞の制約なし
@@ -46,10 +46,10 @@ function loadMWE()
 		mwe = replace(mwe,r"(_|-)","")
 		mwe_reg = Regex(replace(mwe,r"(\.)",".*"))
 		mwe = replace(mwe,r"(\.)","")		#_と先頭の.を消去
-		value = get(reg_hash,mwe,0)
-		if value != 0
-			@show mwe
-		end
+##		value = get(reg_hash,mwe,0)
+##		if value != 0
+##			@show mwe
+##		end
 		reg_hash[mwe] = mwe_reg
 		trie[mwe] = cnt
 		cnt += 1
@@ -105,93 +105,93 @@ mwe_cnt = 0		#マッチしたMWEをカウント
 
 matched_mwe_hash = Dict{UTF8String,Int64}()		#マッチしたMWEをカウントする(k:MWE,v:cnt)
 trie,reg_hash,lex_cnt = loadMWE()
-#lex_cnt = length(reg_hash)
+lex_cnt = length(reg_hash)
 #@show reg_hash
 #@show subtrie(trie, "")	#trieが返ってくる
 
-#BCCWJ_CORE_M-XMLファイルオープン
-#child = readdir(BCCWJ_ROOT)
-#for xfile in child
-##	println("--------------------------")
-#	path = BCCWJ_ROOT*"/"*xfile
-#	doc = open(path) do fp
-#		readall(fp)
-#	end
-#	#xmlパーズ
-#	root_node = xp_parse(doc)
-#	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
-#	for sentence_node in sentence_nodes
-#		sen_cnt += 1
-##		println("--------------------------")
-#		######################################
-#		#		初期化
-#		######################################
-#		origin_sentence = []
-#		yomi_sentence = []
-#		match_array = []
-#		match_yomi = []
-#		yomi = ""
-#		######################################
-#		#		XMLパーズ
-#		######################################
-#		sentence_node = xp_parse(string(sentence_node))
-#		suw_nodes = LibExpat.find(sentence_node,"/sentence//SUW")
-#		for suw_node in suw_nodes
-#			suw = suw_node.elements[1]
-#			push!(origin_sentence,suw)
-#			try
-#				yomi = suw_node.attr["kana"]
-#			catch e
-#				yomi = suw_node.attr["formBase"]
-#			end
-#			push!(yomi_sentence,jcconv.kata2hira(yomi))
-#		end
-#		######################################
-#		#		マッチング
-#		######################################
-##		yomi_sentence = Any["これ","いじょう","","ああ","いえば","こう","いう","のは","やめて"]
-#		match_mwe,yomi_sentence,match_yomi = search_trie(trie,yomi_sentence,reg_hash)
-#		if match_mwe != ""
-#			push!(match_array,match_mwe)
-#		end
-##		@show match_yomi
-#		#他のMWEがマッチする可能性も考慮
-#		while length(match_yomi) != 0		#部分マッチor完全マッチした場合
-#			deleteat!(yomi_sentence, findin(yomi_sentence,match_yomi))	#yomi_sentenceからマッチ要素を削除
-##				@show yomi_sentence
-#			match_mwe,yomi_sentence,match_yomi = search_trie(trie,yomi_sentence,reg_hash)
-#			if match_mwe != ""
-#				push!(match_array,match_mwe)
-#			end
-#		end
-#		if length(match_array) != 0
-#			for matched in match_array
-#				value = get(matched_mwe_hash,matched,0)
-#				if value == 0			#キーが存在しなかった場合
-#					matched_mwe_hash[matched] = 1
-#					mwe_cnt += 1
-#				else
-#					matched_mwe_hash[matched] += 1				
-#					mwe_cnt += 1
-#				end
-#			end
-#			@show origin_sentence
-#			@show yomi_sentence
-#			@show match_array
-#		end
-#		######################################
-#	end
-#end
+##BCCWJ_CORE_M-XMLファイルオープン
+child = readdir(BCCWJ_ROOT)
+for xfile in child
+#	println("--------------------------")
+	path = BCCWJ_ROOT*"/"*xfile
+	doc = open(path) do fp
+		readall(fp)
+	end
+	#xmlパーズ
+	root_node = xp_parse(doc)
+	sentence_nodes = LibExpat.find(root_node,"/mergedSample//sentence")
+	for sentence_node in sentence_nodes
+		sen_cnt += 1
+#		println("--------------------------")
+		######################################
+		#		初期化
+		######################################
+		origin_sentence = []
+		yomi_sentence = []
+		match_array = []
+		match_yomi = []
+		yomi = ""
+		######################################
+		#		XMLパーズ
+		######################################
+		sentence_node = xp_parse(string(sentence_node))
+		suw_nodes = LibExpat.find(sentence_node,"/sentence//SUW")
+		for suw_node in suw_nodes
+			suw = suw_node.elements[1]
+			push!(origin_sentence,suw)
+			try
+				yomi = suw_node.attr["kana"]
+			catch e
+				yomi = suw_node.attr["formBase"]
+			end
+			push!(yomi_sentence,jcconv.kata2hira(yomi))
+		end
+		######################################
+		#		マッチング
+		######################################
+#		yomi_sentence = Any["これ","いじょう","","ああ","いえば","こう","いう","のは","やめて"]
+		match_mwe,yomi_sentence,match_yomi = search_trie(trie,yomi_sentence,reg_hash)
+		if match_mwe != ""
+			push!(match_array,match_mwe)
+		end
+#		@show match_yomi
+		#他のMWEがマッチする可能性も考慮
+		while length(match_yomi) != 0		#部分マッチor完全マッチした場合
+			deleteat!(yomi_sentence, findin(yomi_sentence,match_yomi))	#yomi_sentenceからマッチ要素を削除
+#				@show yomi_sentence
+			match_mwe,yomi_sentence,match_yomi = search_trie(trie,yomi_sentence,reg_hash)
+			if match_mwe != ""
+				push!(match_array,match_mwe)
+			end
+		end
+		if length(match_array) != 0
+			for matched in match_array
+				value = get(matched_mwe_hash,matched,0)
+				if value == 0			#キーが存在しなかった場合
+					matched_mwe_hash[matched] = 1
+					mwe_cnt += 1
+				else
+					matched_mwe_hash[matched] += 1				
+					mwe_cnt += 1
+				end
+			end
+			@show origin_sentence
+			@show yomi_sentence
+			@show match_array
+		end
+		######################################
+	end
+end
 
 #@show matched_mwe_hash
-#########################################
-##	統計量計算
-#########################################
-#ratio_sen = mwe_cnt/sen_cnt
-#ratio_lex = mwe_cnt/lex_cnt
-#mwe_entr = length(matched_mwe_hash)
-#println("MWE(idiom)出現数/BCCWJ総文数 = "*string(mwe_cnt)*"/"*string(sen_cnt)*"="*string(ratio_sen))
-#println("MWE(idiom)出現数/MWE(idiom)総数 = "*string(mwe_entr)*"/"*string(lex_cnt)*"="*string(ratio_lex))
+########################################
+#	統計量計算
+########################################
+ratio_sen = mwe_cnt/sen_cnt
+ratio_lex = mwe_cnt/lex_cnt
+mwe_entr = length(matched_mwe_hash)
+println("MWE(idiom)出現数/BCCWJ総文数 = "*string(mwe_cnt)*"/"*string(sen_cnt)*"="*string(ratio_sen))
+println("MWE(idiom)出現数/MWE(idiom)総数 = "*string(mwe_entr)*"/"*string(lex_cnt)*"="*string(ratio_lex))
 
 
 
