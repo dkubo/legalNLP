9689e67622e86247dd372ca07e4a10fdc94f898e
dkubo <kubo.daiki.kz7@is.naist.jp>
2016-07-14 16:40:07 +0900

    a

--- cuda.sh
+++ cuda.sh
@@ -1,2 +1,9 @@
-export PATH=/home/is/daiki-ku/itc/cuda-5.5/bin:$PATH
-export LD_LIBRARY_PATH=/home/is/daiki-ku/itc/cuda-5.5/lib64:$LD_LIBRARY_PATH
+#export PATH=/home/is/daiki-ku/itc/cuda-5.5/bin:$PATH
+#export LD_LIBRARY_PATH=/home/is/daiki-ku/itc/cuda-5.5/lib64:$LD_LIBRARY_PATH
+#export DYLD_LIBRARY_PATH=/home/is/daiki-ku/itc/cuda-5.5/lib64:$DYLD_LIBRARY_PATH
+export PATH=/home/is/daiki-ku/itc/cuda-6.5/bin:$PATH
+export LD_LIBRARY_PATH=/home/is/daiki-ku/itc/cuda-6.5/lib64:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=/home/is/daiki-ku/itc/cuda-6.5/lib64:$DYLD_LIBRARY_PATH
+export CPATH=/usr/local/cuda/include:$CPATH
+export LIBRARY_PATH=/home/is/daiki-ku/itc/cuda-6.5/lib64:$LIBRARY_PATH
+
--- ntt_si/src/memnn/parse.py
+++ ntt_si/src/memnn/parse.py
@@ -5,54 +5,53 @@ import os
 import numpy as np
 
 def file_list():
-    basename = './tasks_1-20_v1-2/en/'
+    basename = '../../data/tasks_1-20_v1-2/en/'
     files = os.listdir(basename)
     files = sorted([f for f in files if '.txt' in f])
-    files = [(int(f.split('_')[0].replace('qa', '')) ,f) for f in files]
+    files = [(int(f.split('_')[0].replace('qa', '')) ,f) for f in files]	#:q_id:qa1→1
     train_list = []
     test_list = []
+    #trainとtestに分割する(ファイル名から)
     for q_id, filename in files:
         if '_train' in filename:
             train_list.append((q_id, basename + filename))
         else:
             test_list.append((q_id, basename + filename))
-    train_filenames = dict(train_list)
-    test_filenames = dict(test_list)
+    train_filenames = dict(train_list)		#k:q_id,fname
+    test_filenames = dict(test_list)			#k:q_id,fname
     return train_filenames, test_filenames
-# print train_filenames
 
-def r(task_id, filename, vocab):
+
+def r(task_id, filename, vocab):		#filename:ファイルへの相対パス
     batch_list = []
     item_fact = []
     item_qa = []
-    lines = [l for l in open(filename)]
-    for i, l in enumerate(lines):
-        lsplit = l.strip().split('\t')
-        text = lsplit[0].replace('?', ' ?').replace('!', ' !').replace('.', ' .').lower()
-        s_id = int(text.split()[0])
-        sentence_split = text.split()[1:]
-        sentence_str = ' '.join(sentence_split)
-        # print len(lsplit)
-
-        if (s_id == 1 and i != 0) or i == len(lines):
-            # print '***'
-            # print item_fact
-            # print item_qa
+    lines = [l for l in open(filename)]					#filenameを開いて、一行ごと配列に格納
+    for i, l in enumerate(lines):								#インデックス(i=0,1,...)付きでループ
+        lsplit = l.strip().split('\t')					#質問文にくっついている情報を分離
+        text = lsplit[0].replace('?', ' ?').replace('!', ' !').replace('.', ' .').lower()	#文の末尾の記号正規化
+        s_id = int(text.split()[0])							#文id取得→数値型に変換
+        sentence_split = text.split()[1:]				#文と付随する情報を空白で分かち書きした配列
+        sentence_str = ' '.join(sentence_split)	#文を空白で分かち書きした文字列
+				#文セットの初め(s_id=1)かつそれが一番初めの行でない(i!=0) or 文末(i == len(lines))
+        if (s_id == 1 and i != 0) or i == len(lines):			#lines:1ファイル内から読み込んだ一文ごとの配列
+#            print '***'
+#            print item_fact
+#            print item_qa
             batch_list.append((item_fact, item_qa))
             item_fact = []
             item_qa = []
-
-        if len(lsplit) == 1:
+        print lsplit
+        if len(lsplit) == 1:		#質問文ではない普通の文のとき
             sentence_idx = build_vocab(vocab, sentence_split)
             item_fact.append((task_id, s_id, sentence_idx, sentence_str))
-        else:
-            # print lsplit
-
+        else:			#質問文のとき
             # answer = lsplit[1].lower().split(',')
-            answer = [lsplit[1].lower()]
-            # print answer
+#            answer = [lsplit[1].lower()]
+#            print "answer:"+answer[0]
             answer_hint = map(int, lsplit[2].split())
-            sentence_idx = build_vocab(vocab, sentence_split)
+#            print answer_hint
+            sentence_idx = build_vocab(vocab, sentence_split)	#vocab:Hash
             answer_idx = build_vocab(vocab, answer)
             item_qa.append((task_id, s_id, sentence_idx, sentence_str, answer_idx, answer, answer_hint))
         # print s_id, 
@@ -77,7 +76,7 @@ def build_dataset():
     vocab = {}
     train_dataset = []
     test_dataset = []
-    for task_id in range(1, 20 + 1):
+    for task_id in range(1, 20 + 1):		#1〜20
         train_filename = train_filenames[task_id]
         train_batch_list = r(task_id, train_filename, vocab)
         for fact, qa in train_batch_list:
