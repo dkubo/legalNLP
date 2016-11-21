ed7d8aac1eaea3f7e9145e196e4d958560c32154
dkubo <kubo.daiki.kz7@is.naist.jp>
2016-11-21 20:45:40 +0900

    fixed

--- MWE/src/dict_match.rb
+++ MWE/src/dict_match.rb
@@ -17,7 +17,8 @@ def getmwe(dict)
 	data_hash = JSON.parse(File.read(dict))
 
 	data_hash.each{|mweid, value|
-		mwelist.push([value["suw_lemma"], value["left"], value["meaning"]])
+		mwelist.push([mweid, value["suw_lemma"], value["left"], value["meaning"]])
+
 		for mwe in value["variation_lemma"]
 			mwelist.push([mweid, mwe, value["left"], value["meaning"]])
 		end
@@ -29,12 +30,9 @@ end
 
 # KTCコーパスを一文ずつに分解する
 def splitSentence(file)
-	sent_hash = {}
-	suw_list = []
-	pre_sentid = ""
-	sentid = ""
-	cnt = 0
+	sent_hash, suw_list, pre_sentid, sentid = {}, [], "", ""
 	corpus = open(file, 'r')
+
 	corpus.each_line{|l|
 		l = l.chomp.split("\t")
 		if /# SENT-ID: / =~ l[0]
@@ -48,8 +46,8 @@ def splitSentence(file)
 			suw_list.push(l) unless l == []
 		end
 	}
+
 	sent_hash[pre_sentid] = suw_list
-	# p sent_hash.length 	# 6039 (train)
 	return sent_hash
 end
 
@@ -64,6 +62,7 @@ def getConst()
 		}
 		consthash[sig] = c_list
 	}
+
 	file_2 = open("const2.tsv",'r')
 	file_2.each_line{|l|
 		new_list = []
@@ -77,12 +76,14 @@ def getConst()
 		}
 		consthash[sig] = new_list
 	}
+
 	return consthash
 end
 
 # カンマ区切りされているデータがある⇒分割
 def makeArray(v)
 	sentlist, lemlist, poslist, vari1, vari2, sentence, lemma, sentpos = [], [], [], [], [], [], [], []
+
 	v.each{|line|
 		sentlist.push(line[8].split(",", -1))
 		lemlist.push(line[9].split(",", -1))
@@ -98,6 +99,7 @@ def makeArray(v)
 				vari2 += line[12].split(",", -1)
 		end
 	}
+
 	sentlist.flatten!
 	lemlist.flatten!
 	poslist.flatten!
@@ -115,21 +117,26 @@ def makeArray(v)
 		end
 		sentpos.push([poslist[i], vari1[i], vari2[i]])
 	end
+
 	return sentence, lemma, sentpos
 end
 
-def splitCont(mwe, startlen, endlen, start_idx, sentence)
-	return sentence[0..start_idx-1].join(), sentence[start_idx..start_idx+mwe.length-1].join(), sentence[start_idx+mwe.length..-1].join()
+def splitCont(mwe, start_idx, sentence)
+	precont, matched, postcont = sentence[0..start_idx-1].join(), 
+																sentence[start_idx..start_idx+mwe.length-1].join(), 
+																sentence[start_idx+mwe.length..-1].join()
+	startlen, endlen = precont.length+1, precont.length + matched.length
+	return precont, matched, postcont, startlen, endlen
 end
 
 # MWEID, 文ID, 開始文字位置, 終了文字位置, 前文脈, 対象表現, 後文脈, MEANING
-def	writeCSV(fname)
+def	writeCSV(fname, outdata)
 	file = CSV.open(fname, 'w')
-	$outdata.each{|data| file.puts data }
+	outdata.each{|data| file.puts data }
 	file.close
 end
 
-def matching(mweid, mwe, leftconst, meaning, s_id, sentence, lemma, sentpos, consthash)
+def matching(mweid, mwe, leftconst, meaning, s_id, sentence, lemma, sentpos, consthash, outdata)
 	m_frg, leftconst, totallen = 0, leftconst[0].scan(/.{1,2}/), 0
 
 	lemma.each_with_index do |lempart, idx|  	# sentence loop
@@ -154,15 +161,20 @@ def matching(mweid, mwe, leftconst, meaning, s_id, sentence, lemma, sentpos, con
 
 			# MWEが完全にマッチしたとき
 			if m_frg == 1
-				p mwe
-				p sentence
-				precont, matched, postcont = splitCont(mwe, startlen, endlen, start_idx, sentence)		# 出力用整形
-				$outdata.push([mweid, s_id, startlen.to_s, endlen.to_s, precont, matched, postcont, meaning])
+				# precont, matched, postcont = splitCont(mwe, start_idx, sentence)		# 出力用整形
+				precont, matched, postcont, startlen, endlen = splitCont(mwe, start_idx, sentence)		# 出現形の文に対してのstartlen等を取得
+				# p matched
+				# p sentence
+				# p meaning
+				# p startlen, endlen
+				outdata.push([mweid, s_id, startlen.to_s, endlen.to_s, precont, matched, postcont, meaning])	# startlen, endlen: 標準形の文に対してのもの
+				# outdata.push([leftconst.join(), s_id, startlen.to_s, endlen.to_s, precont, matched, postcont, meaning])
 				m_frg = 0
 			end
 		end
 		totallen += lempart.length
 	end
+	return outdata
 end
 
 # 制約リスト, 確認対象の品詞等
@@ -180,8 +192,8 @@ def constCheck(constlist, sentleft, m_frg)
 		}
 		if check == 1 then
 			m_frg = 1
-			p "---------------------------"
-			p const
+			# p "---------------------------"
+			# p const
 			break
 		else
 			m_frg = 0
@@ -191,21 +203,24 @@ def constCheck(constlist, sentleft, m_frg)
 end
 
 
-def proc(pathtocorp, mwelist, consthash)
+def proc(pathtocorp, mwelist, consthash, outdata)
 	# open the corpus
 	sent_hash = splitSentence(pathtocorp)	# train: 6039, test: 2837, dev: 1119
 
 	sent_hash.each{|s_id, v|
 		sentence, lemma, sentpos = makeArray(v)
 		mwelist.each{|mweid, mwe, leftconst, meaning|
-			next if mwe.length == 1
-			matching(mweid, mwe, leftconst, meaning, s_id, sentence, lemma, sentpos, consthash)
+			next if mwe.join().length == 1
+			outdata = matching(mweid, mwe, leftconst, meaning, s_id, sentence, lemma, sentpos, consthash, outdata)
 		}
 	}
+	return outdata
 end
 
 def main()
-	$outdata = []		# global
+	outdata = []
+	# $outdata = []		# global
+
 	# 制約のリスト取得
 	consthash = getConst()
 
@@ -215,12 +230,12 @@ def main()
 
 	# 辞書for文
 		for type in ["train", "test", "dev"] do
-			pathtocorp = "../data/20161007/corpus/ud/ja_ktc-ud-"+"#{type}"+"-merged.conll"
-			proc(pathtocorp, mwelist, consthash)
+			pathtocorp = "../data/20161007/corpus/ud/ja_ktc-ud-#{type}-merged.conll"
+			outdata = proc(pathtocorp, mwelist, consthash, outdata)
 		end
 
 	# csv書き込み
-	writeCSV("matced_mwe.csv")
+	writeCSV("matced_mwe.csv", outdata)
 	# writeCSV("matced_mwe_oneword.csv")
 end
 
