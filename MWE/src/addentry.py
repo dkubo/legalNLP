62f9646c6823df8f4c620e3f821d025824817439
dkubo <kubo.daiki.kz7@is.naist.jp>
2016-11-28 11:55:58 +0900

    update

---
+++ MWE/src/addentry.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+
+import json
+
+DICT="../result/tsutsuji_dic_20161121.json"
+ADDLIST="../result/candidate_edit.txt"
+
+def opendic(path):
+	with open(path, 'r') as f:
+		jsondata = json.load(f)
+	return jsondata
+
+def openaddlist(path):
+	with open(path, 'r') as f:
+		for line in f:
+			mwe, mweidlist = f.rstrip().split(",")
+
+
+def main():
+	jsondic = opendic(DICT)
+	print(jsondic)
+
+	# openaddlist(ADDLIST)
+
+if __name__ == '__main__':
+	main()
